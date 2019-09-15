2fb0d55be4f73fc6ee47ed139b1908c106f19c04
Cory Westerfield <cory.westerfield@gmail.com>
2019-09-15 09:12:06 -0600

    Add necessary skeleton sections for app

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,68 @@
 module Main exposing (main)
 
-import Html exposing (text)
+import Browser
+import Html exposing (Html, div, text)
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { thing : String }
+
+
+type Msg
+    = NoOP
+
+
+init : () -> ( Model, Cmd Msg )
+init () =
+    ( initialModel, Cmd.none )
+
+
+initialModel : Model
+initialModel =
+    { thing = "Hello" }
+
+
+
+-- UPDATE
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NoOP ->
+            ( model, Cmd.none )
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div [] [ text "Hello" ]
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+
+-- MAIN
 
 
 main =
-    text "Hello world"
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
